2d9f28641892236767bce8e8acae772f3d45befa
Deryl Rodrigues <derylrodrigues@Deryls-MacBook-Pro.local>
2016-03-04 16:39:41 -0500

    [DERYL] COMMUNICATION DEMO CODE

---
+++ app/src/main/java/com/derylrodrigues/firebasesamplecode/MainActivity.java
@@ -0,0 +1,90 @@
+package com.derylrodrigues.firebasesamplecode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.firebase.client.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private static final String FIREBASE_DB = "https://madscienceapp.firebaseIO.com/";
+
+    private EditText key;
+    private EditText value;
+    private Button save;
+
+    private EditText fetchKey;
+    private Button fetch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+
+        key = (EditText) findViewById(R.id.Key);
+        value = (EditText) findViewById(R.id.Value);
+        save = (Button) findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveValue(key.getText().toString(), value.getText().toString());
+            }
+        });
+
+        fetchKey = (EditText) findViewById(R.id.FetchKey);
+        fetch = (Button) findViewById(R.id.fetch);
+        fetch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getValue(fetchKey.getText().toString());
+            }
+        });
+    }
+
+
+    private void saveValue(String key, String value) {
+        Firebase ref = new Firebase(FIREBASE_DB);
+        Firebase usersRef = ref.child(key);
+        usersRef.setValue(value, new Firebase.CompletionListener() {
+            @Override
+            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+                if (firebaseError != null) {
+                    Log.d(TAG, "Data could not be saved. " + firebaseError.getMessage());
+                } else {
+                    Log.d(TAG, "Data saved successfully.");
+                }
+            }
+        });
+    }
+
+    private void getValue(String key) {
+        Log.d(TAG, "Get Value for Key - " + key);
+        Firebase ref = new Firebase(FIREBASE_DB + key);
+        Query queryRef = ref.orderByKey();
+        queryRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                // snapshot contains the key and value
+                Toast.makeText(MainActivity.this, "Key - " + snapshot.getKey() + " - Value - " + snapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.e(TAG, firebaseError.getMessage());
+                Log.e(TAG, firebaseError.getDetails());
+            }
+        });
+    }
+}
